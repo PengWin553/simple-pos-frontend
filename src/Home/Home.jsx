0dfa7447d4a93ee8ec9ac5b3846cc0e5d0a149a0
EdisonP <133362311+edions@users.noreply.github.com>
2024-08-15 12:15:58 +0800

    template-ts

--- src/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-    return(
-        <div className="crud-content-container">
-            <h2 className="page-title">Welcome to The Homepage</h2>
-        </div>
-    )
-}
-
-export default Home;
